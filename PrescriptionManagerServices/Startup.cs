1eff1458a807e311f53766540e0dc096c8f2775d
Daren May <daren.may@gmail.com>
2017-12-01 15:00:18 -0700

    Added DbContext service registration to startup

--- PrescriptionManagerServices/Startup.cs
+++ PrescriptionManagerServices/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using PrescriptionManagerServices.Models;
 
 namespace PrescriptionManagerServices
 {
@@ -23,6 +24,7 @@ namespace PrescriptionManagerServices
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ContosoMedicalDBContext>();
             services.AddMvc();
         }
 
---
+++ PrescriptionManagerServicesClient/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PrescriptionManagerServicesClient
+{
+    public class Class1
+    {
+    }
+}
